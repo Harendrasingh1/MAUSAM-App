25bd0bec02a9515653213562e7d5bea8d47d0bf6
Harendra Singh <IIB2021015@iiita.ac.in>
2024-08-03 15:47:00 +0530

    Update Credit.js

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -7,7 +7,7 @@ export default function Credit() {
       Coded by
       <a href="https://www.linkedin.com/in/harendra8587/"
         > Harendra Singh</a
-      >:- <a href="https://github.com/Harendrasingh1">Github Profile</a> Thank You!
+      >, <a href="https://github.com/Harendrasingh1">Github Profile</a> Thank You!
     </p>
   </div>
   );
